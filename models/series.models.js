4e0aa437e30b52f167970721044c044bb675e494
Robson <robsonoliveira@gmail.com>
2020-07-10 18:00:05 -0300

    user and serie model done

---
+++ models/series.models.js
@@ -0,0 +1,47 @@
+ 
+const mongoose = require('mongoose');
+const { Schema, model } = mongoose;
+
+const userSchema = new Schema(
+    {
+      username: {
+        type: String,
+        trim: true,
+        required: [true, 'Username is required.'],
+        unique: true
+      },
+      passwordHash: {
+        type: String,
+        required: [true, 'Password is required.']
+      }
+    },
+    {
+      timestamps: true
+    }
+  );
+
+const serieSchema = new Schema(
+  {
+    title: String, //título
+    cast: [], //elenco
+    genre: String, // gênero
+    image: String, //foto de capa
+    description: String, //descrição
+    rate: { //avaliação
+        type: Number,
+        min: 0,
+        max: 10
+    } 
+  },
+  {
+    timestamps: true
+  }
+);
+
+
+module.exports = model('User', userSchema);
+
+// every new serie will be saved in the "series" collection in the database:
+
+module.exports = model('Serie', serieSchema);
+module.exports = model('User', userSchema);
