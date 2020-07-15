31ae167f2e648a1cfbf895d59abecd33c5cc4ee9
Erick-34 <erickoliveira2060@gmail>
2020-07-15 12:56:48 -0300

    Erick-6

--- routes/series.routes.js
+++ routes/series.routes.js
@@ -11,31 +11,31 @@ router.get("/series", async (req, res) => {
   }
 });
 
-router.get('/serie/:serieId', async (req, res) => {
+router.get("/serie/:serieId", async (req, res) => {
   try {
     const serieId = await Serie.findById(req.params.serieId);
-    res.render('details', { serie: serieId });
+    res.render("details", { serie: serieId });
   } catch (err) {
     throw new Error(err);
   }
 });
 
-router.get('/serie/create', (req, res) => {
-  res.render('create-form');
+router.get("/create/serie", (req, res) => {
+  res.render("create-form");
 });
 
-router.post("/serie/create", async (req, res, next) => {
+router.post("/create/serie", async (req, res, next) => {
   const data = req.body;
+
   try {
     const result = await Serie.create(data);
-    //console.log(result);
     res.redirect("/series");
   } catch (err) {
     throw new Error(err);
   }
 });
 
-router.get("/serie/:id/edit", async (req, res, next) => {
+router.get("/idit/serie/:id", async (req, res, next) => {
   const { id } = req.params;
   try {
     const serie = await Serie.findById(id);
@@ -45,26 +45,24 @@ router.get("/serie/:id/edit", async (req, res, next) => {
   }
 });
 
-router.post("/serie/:id/edit", async (req, res, next) => {
+router.post("/idit/serie/:id", async (req, res, next) => {
   const { id } = req.params;
   const data = req.body;
   try {
     const updateResult = await Serie.updateOne({ _id: id }, { $set: data });
-    res.redirect("/series");
+    res.redirect("/series", { updateResult });
   } catch (err) {
     throw new Error(err);
   }
 });
 
-router.get("/serie/delete", (req, res) =>
-  res.render("deleteConfirme")
-);
+router.get("/delete/serie/:id", (req, res) => res.render("deleteConfirme"));
 
-router.get("/serie/:id/delete", async (req, res, next) => {
+router.get("/delete/serie/:id", async (req, res, next) => {
   const { id } = req.params;
   try {
     const deletionResult = await Serie.deleteOne({ _id: id });
-    console.log(deletionResult)
+    console.log(deletionResult);
     res.redirect("/series");
   } catch (err) {
     throw new Error(err);
